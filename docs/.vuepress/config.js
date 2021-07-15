5cbcf68832a906c9111491f8ca76caee08db5375
vayne1q <1789502923@qq.com>
2021-07-16 00:34:41 +0800

    Build project structure

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,29 +1,42 @@
 module.exports = {
-  title: 'Sen Li\'s blog',
-  description: '前进之路',
+  title: '进阶文档',
+  description: '前端进阶之路',
+  repo: 'https://github.com/vayne1Q/VuePressBlog.git', //仓库地址
   head: [ // 注入到当前页面的 HTML <head> 中的标签
     ['link', {
       rel: 'icon',
       href: '/logo.jpg'
     }], // 增加一个自定义的 favicon(网页标签的图标)
-  ],
-  base: '/', // 这是部署到github相关的配置
+  ], // 这是部署到github相关的配置
+  lastUpdated: 'Last Updated',
   markdown: {
     lineNumbers: false // 代码块显示行号
   },
   themeConfig: {
     nav: [ // 导航栏配置
       {
-        text: '前端基础',
-        link: '/accumulate/'
+        text: 'JS',
+        link: '/foundation/'
       },
       {
-        text: '算法题库',
-        link: '/algorithm/'
+        text: 'Vue',
+        link: '/vue/'
       },
       {
-        text: '微博',
-        link: 'https://baidu.com'
+        text: 'React',
+        link: '/react/'
+      },
+      {
+        text: '工程化',
+        link: '/engineering/'
+      },
+      {
+        text: '源码篇',
+        link: '/source/'
+      },
+      {
+        test: '代码规范',
+        link: '/standard/'
       }
     ],
     sidebar: 'auto', // 侧边栏配置
